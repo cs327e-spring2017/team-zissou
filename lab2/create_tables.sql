ffdd2dc9c7389ae0abdeccc44a327ae93a0e4bdd
jake-westfall <jake.westfall@utexas.edu>
2017-02-15 19:24:36 -0600

    Revise create_tables.sql; Draft other scripts

--- lab2/create_tables.sql
+++ lab2/create_tables.sql
@@ -4,42 +4,45 @@ create database imdb;
 
 create table Movies(
 	movie_id int primary key,
-	title varchar(50),
+	title text,
 	year int,
 	num int,
-	location varchar(5),
-	language varchar(20));
+	type int,
+	location text,
+	language text);
 
 create table Genres(
 	genre_id int primary key,
-	genre varchar(20));
+	genre text);
 
 create table Keywords(
 	keyword_id int primary key,
-	keyword varchar(20));
+	keyword text);
 
 create table Series(
 	series_id int primary key,
 	movie_id int not null references Movies(movie_id),
-	name varchar(50),
+	name text,
 	season int,
 	episode_num int);
 
 create table Actors(
 	actor_id int primary key,
-	last_name varchar(20),
-	first_name varchar(20),
-	mid_name varchar(20),
-	gender varchar(1),
+	last_name text,
+	first_name text,
+	mid_name text,
+	gender int,
 	which_one int);
 
-create table Cast(
+create table Casts(
 	cast_id int primary key,
-	movie_id int not null references Movies(movie_id),
-	actor_id int not null references Actors(actor_id),
-	series_id int not null references Series(series_id));
+	movie_id int references Movies(movie_id),
+	actor_id int references Actors(actor_id),
+	series_id int references Series(series_id),
+	character text,
+	position int);
 
 create table Characters(
 	char_id int primary key,
 	actor_id int not null references Actors(actor_id),
-	char_name varchar(20));
+	char_name text);
